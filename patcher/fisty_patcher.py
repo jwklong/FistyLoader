061b87a1b2616227232250e4086d201f3e7d7ba8
Darxoon <darxoon@outlook.com>
2024-08-11 16:38:41 +0200

    Initial Commit

---
+++ patcher/fisty_patcher.py
@@ -0,0 +1,54 @@
+from sys import stderr
+from pefile import PE, SectionStructure
+
+def add_section_header(pe: PE, section_size: int):
+    print("Increasing section count...")
+    pe.FILE_HEADER.NumberOfSections += 1
+    
+    print(f"Increasing image size by 0x{section_size:x}...")
+    pe.OPTIONAL_HEADER.SizeOfImage += section_size
+    
+    prev_section: SectionStructure = pe.sections[-1]
+    
+    print("Creating section .fisty...")
+    section = SectionStructure(PE.__IMAGE_SECTION_HEADER_format__, pe=pe)
+    section.Name = ".fisty".encode()
+    section.Misc = section_size
+    section.Misc_PhysicalAddress = section_size
+    section.Misc_VirtualSize = section_size
+    section.VirtualAddress = prev_section.VirtualAddress + prev_section.SizeOfRawData
+    section.SizeOfRawData = section_size
+    section.PointerToRawData = prev_section.PointerToRawData + prev_section.SizeOfRawData
+    section.PointerToRelocations = 0
+    section.PointerToLinenumbers = 0
+    section.NumberOfRelocations = 0
+    section.NumberOfLinenumbers = 0
+    section.Characteristics = 0x60000000
+    
+    section.set_file_offset(0x3d0)
+    pe.sections.append(section)
+    pe.__structures__.append(section)
+
+def add_section_content(filename: str, content: bytes):
+    with open(filename, 'ab') as f:
+        f.write(content)
+
+def main():
+    with open('custom_code.bin', 'rb') as f:
+        section_content = f.read()
+    
+    pe = PE("World of Goo 2.exe")
+    add_section_header(pe, len(section_content))
+    
+    print("Writing out.exe...")
+    pe.write("out.exe")
+    add_section_content("out.exe", section_content)
+    
+    print("Done.")
+    
+    # Load it again and print info
+    pe = PE("out.exe")
+    pe.print_info()
+
+if __name__ == '__main__':
+    main()
