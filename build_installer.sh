732caef646d6478e3c4ed778fd9820b89ae4b3ea
jwklong <72522395+jwklong@users.noreply.github.com>
2025-08-29 10:07:03 +0100

    Update build_installer.sh

--- build_installer.sh
+++ build_installer.sh
@@ -18,6 +18,22 @@ fi
 
 pip install -r requirements.txt
 
+assemble() {
+    mkdir -p patch/build
+    nasm patch/main.s -f elf64 -o patch/build/main.o
+}
+
+link() {
+    ld -o patch/build/custom_code.o --oformat elf64-x86-64 patch/build/main.o -T main.ld
+}
+
+copy() {
+    objcopy patch/build/custom_code.o -O binary patcher/custom_code.bin
+    objcopy patch/build/custom_code.o --only-keep-debug patcher/custom_code_symbols.o
+}
+
+assemble && link && copy
+
 nasm patch/main.s -o patcher/custom_code.bin
 pyinstaller -F patcher/install.py --add-data patcher/custom_code.bin:. --recursive-copy-metadata readchar --clean
 
