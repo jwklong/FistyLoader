8ca99d89c3ed63eb00e5149a2915e9427c9f32b2
jwklong <72522395+jwklong@users.noreply.github.com>
2025-08-29 11:09:01 +0100

    Update build_installer.sh

--- build_installer.sh
+++ build_installer.sh
@@ -18,6 +18,6 @@ fi
 
 pip install -r requirements.txt
 
-pyinstaller -F patcher/install.py --add-data patcher/custom_code.bin:. --recursive-copy-metadata readchar --clean
+pyinstaller -F patcher/install.py --add-data patcher/custom_code.bin:. --add-data patcher/custom_code_symbols.o:. --recursive-copy-metadata readchar --clean
 
 echo Done.
